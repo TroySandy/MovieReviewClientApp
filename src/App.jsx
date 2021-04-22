2bbaac6bf3c0d14370dfb83913476446a02d8ca6
Alexander Mason <tantrex93@gmail.com>
2021-04-22 13:30:16 -0400

    file rename 2/2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Switch } from "react-router-dom";
-import Login from "./components/auth/login2";
+import Login from "./components/auth/Login";
 import Register from "./components/auth/register2";
 import Home from "./components/home/Home";
 import Nav from "./components/site/Nav";
